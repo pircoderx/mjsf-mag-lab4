1ed8bf7ac986a59cf26b6f04926b6b18b1927a37
Artem Pirunov <psycoderx@gmail.com>
2024-10-27 09:01:07 +0200

    Init the repo

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import '@/assets/main.scss'
+
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
+import directivesPlugin from '@/plugins/directives';
+import i18n from '@/plugins/i18n'
+// import App from './App.vue'
+import DemoI18n from "@/DemoI18n.vue";
+import router from './router'
+
+const app = createApp(DemoI18n)
+
+app.use(createPinia())
+app.use(router)
+app.use(directivesPlugin);
+app.use(i18n, {
+    greetings: {
+        hi: 'Привіт!'
+    }
+});
+
+app.mount('#app')
